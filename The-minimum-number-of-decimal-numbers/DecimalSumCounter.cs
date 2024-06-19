29b4a44e3b54c6d5b38345cd52674d653a464228
Famez1 <a.plyakin@sh.dunice.net>
2024-06-19 10:30:09 +0300

    Сделал калькулятор десятичных числе

---
+++ The-minimum-number-of-decimal-numbers/DecimalSumCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_minimum_number_of_decimal_numbers
+{
+    internal class DecimalSumCounter
+    {
+        public static int CalculationOfCountDecimalCombinations(string n)
+        {
+            int maxDigit = 0;
+
+            foreach (char c in n)
+            {
+                if (char.IsDigit(c))
+                {
+                    int digit = int.Parse(c.ToString()); 
+
+                    if (digit > maxDigit)
+                    {
+                        maxDigit = digit;
+                    }
+                }
+            }
+
+            return maxDigit;
+        }
+    }
+}
---
+++ The-minimum-number-of-decimal-numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+
+using The_minimum_number_of_decimal_numbers;
+
+Console.WriteLine(DecimalSumCounter.CalculationOfCountDecimalCombinations("123")); 
